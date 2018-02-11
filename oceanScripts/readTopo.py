6fd0d00b363ec7b3ddac01dd9a5338c060987d4a
rbarcklay <rachel.barcklay@gmail.com>
2018-02-11 15:28:38 -0800

    rlb: refractive index as function of time

--- oceanScripts/readTopo.py
+++ oceanScripts/readTopo.py
@@ -12,7 +12,7 @@ from datetime import timedelta
 product = reduce((lambda x, y: x * y), [1, 2, 3, 4])
 #from netCDF4 import Dataset
 
-df = pandas.read_csv("OSMC_flattened_eff7_eca9_6785.csv", parse_dates=[0], header=0,delimiter=",")
+df = pandas.read_csv("OSMC_flattened_eff7_eca9_6785.csv", parse_dates=[0], header=0,delimiter=",") #data for 32.3N - 144.6E
 
 
 def getColData(column):
@@ -87,17 +87,36 @@ def get4Times(day):
 	return datetimes
 	'''
 
+def refractiveIndexList(salList, tempList):
+	rIndices = []
+	l = 500 #nm
+	if len(salList) == len(tempList):
+		for i in range(len(salList)):
+			t = float(tempList[i])
+			s = float(salList[i])
+			if (not numpy.isnan(t)) and (not numpy.isnan(s)):
+				rIndices.append(1.3247 - 2.5e-6*t**2 + s*(2e-4 - 8e-7*t) + 3300*l**(-2) - 3.2e7*l**(-4))
+			else:
+				rIndices.append(numpy.nan)
+	return rIndices
 
 
 
 def main():
 	startDate = df['time'][0]
 
-	plt.subplot(211)
-	plt.scatter(getColData('ztmp')[1], getColData('ztmp')[0])
+	salData = getColData('zsal')[0]
+	tempData = getColData('ztmp')[0]
+	times = getColData('zsal')[1]
 
-	plt.subplot(212)
-	plt.scatter(getColData('zsal')[1], getColData('zsal')[0])
+	# plt.subplot(311)
+	# plt.scatter(times, tempData)
+
+	# plt.subplot(312)
+	# plt.scatter(times, salData)
+
+	# plt.subplot(313)
+	plt.scatter(times, refractiveIndexList(salData, tempData))
 	plt.show()
 
 if __name__ == "__main__":
