ca7457f0061168b993ebb2a7bfeaf483583ff9b2
carlacupcake <cbecker@g.hmc.edu>
2018-02-10 23:20:10 -0800

    cjb: test

--- model/radio.py
+++ model/radio.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 # Author: cbecker@g.hmc.edu
 
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#-----------------------------------------------------------------------#
+'''
 TODO:
 learn about fft and ifft
-get_num_refl(angle_of_incidence, distance)
-calc_noise(weather_severity, N)
 fresnel(ocean_index_of_refr, iono_index_of_refr, angle_of_incidence, f0_w)
 dispersion(f0_w_post_Fresnel)
-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'''
+#-----------------------------------------------------------------------#
 
 
 '''
@@ -36,9 +36,10 @@ Notes:
 '''
 
 import os
-import numpy
-import math
+import numpy as numpy
+import math as math
 import constants as constants
+import ionosphere as iono_dict 
 
 def get_num_refl(angle_of_incidence, distance):
     d = distance
@@ -48,32 +49,45 @@ def get_num_refl(angle_of_incidence, distance):
     N = d * math.tan(a) / (h - z)
     return N
 
-def calc_noise(weather_severity, N):
+def calc_noise(weather_severity, N, array_length):
+    base_stdev = array_length/100.0
+    wc = 1/weather_severity # weather contribution
+    Nc = math.sqrt(N) # Reflection number contribution
+    stdev = base_stdev * wc * Nc
+    noise = numpy.random.normal(0, stdev, array_length)
+    return noise
+
+def fresnel(ocean_index_of_refr, angle_of_incidence, f0_w):
+    a = angle_of_incidence
+    print constants.electron_charge
+
 
     
-
+    
+#-----------------------------------------------------------------------#
 
 if __name__ == '__main__':
+
+    print constants.electron_charge
 
     # Read in file arguments
     args = sys.argv[1:]
     ocean_index_of_refr = args[1]
-    iono_index_pf_refr = args[2]
-    angle_of_incidence = args[3]
-    distance =  args[4] # distance on Earth's surface to be travelled
-    weather_severity = args[5]
+    angle_of_incidence = args[2]
+    distance =  args[3] # distance on Earth's surface to be travelled
+    weather_severity = args[4]
     input_file = args[5] # input signal
 
+    # Get input data from .csv
     # Decompose the input signal, f0_t, to it's Fourier components f0_w
-    input_data = numpy.genfromtxt(input_file, delimiter = ',')
-    time_vector = input_data
+    [time, f0_t] = numpy.genfromtxt(input_file, delimiter = ',')
     f0_w = fft(f0_t)
 
     # Number of reflections
     N = get_num_refl(angle_of_incidence, distance)
 
     # Calculate noise vector to be added to output signal
-    noise = calc_noise(weather_severity, N)
+    noise = calc_noise(weather_severity, N, array_length)
     
     # For each f0_w, compute the output after Fresnel and dispersion
     f0_w_post_Fresnel = fresnel(ocean_index_of_refr, iono_index_of_refr, angle_of_incidence, f0_w, constants)
@@ -81,10 +95,12 @@ if __name__ == '__main__':
     f_w = f0_w_post_dispersion
 
     # Compose output signal and add in accumulated noise
+    # Export output data to .csv
     f_t_clean = ifft(f_w)
-    f_t = f_t + noise
+    f_t = f_t_clean + noise
+    output_file = numpy.savetxt('radio_output.csv', [time, f_t], delimiter = ',')
 
-    return f_t
+    return output_file
 
     
 
