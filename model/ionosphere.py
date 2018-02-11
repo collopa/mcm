9e4d12a7394b6d0139fd84bd32b9f0eab7effba5
Colin Adams <collopa@Colins-MacBook-Pro.local>
2018-02-10 22:48:04 -0800

    cma; fix figures

--- model/ionosphere.py
+++ model/ionosphere.py
@@ -15,10 +15,15 @@ import constants as c
 ############################
 # Functions
 #############################
-def N_z(z, angle_inc, recomb_coeff):
+def N_z(z, angle_inc, sigma):
     """
         Gives charge number of electrons per m^3
     """
     n0 = 1
+    # sigma = 4.032e18
     kappa = c.gas_const*c.T_tropo/(c.p0 * c.grav)
-    return kappa
+    z0 = kappa*np.log(sigma*kappa*c.p0) 
+    inside = 0.5*(1 - (z - z0)/kappa - 1./np.cos(angle_inc) * np.exp(-(z - z0)/kappa))  
+    return n0 * np.exp(inside)
+
+
