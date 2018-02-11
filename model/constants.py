7cfa8c40abc299f8f3ea636afc21d6405bd09564
carlacupcake <cbecker@g.hmc.edu>
2018-02-10 16:24:13 -0800

    cjb: added to assumptions

--- model/constants.py
+++ model/constants.py
@@ -9,17 +9,23 @@ T_thermo = 1773 # [Kelvin]
 T_iono = 350 # [Kelvin]
 
 air_dielec_const = 1.000 # [Coulomb^2 Second^2 Meter^-3 Kilogram^-1]
+MW_air = 28.95 # [Grams Mole^-1]
 
 #---------------------------------------------------------------#
 
 def get_default():
     return {
         'temperature':{
-                 'T_therm': T_therm,
-                 'T_meso': T_meso,
-                 'T_tropo': T_tropo,
-                 'T_strato': T_strato ,
-                 'T_iono': T_iono
+            'T_therm': T_therm,
+            'T_meso': T_meso,
+            'T_tropo': T_tropo,
+            'T_strato': T_strato ,
+            'T_iono': T_iono
+            },
+        'atmosphere':{
+            'air_dielec_const': air_dielec_const,
+            'MW_air': MW_air
+            }
 }
 
 #---------------------------------------------------------------#
