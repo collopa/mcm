0acf56ead85986141e8ff8a7971dfe9b2556abb6
carlacupcake <cbecker@g.hmc.edu>
2018-02-12 16:08:55 -0800

    cjb: updates to tex

--- model/run_radio.sh
+++ model/run_radio.sh
@@ -8,4 +8,4 @@ ANGLE=30 # angle of incidence (from parallel to Earth) in degrees
 WEATHER=0 # weather severity on a scale of 0-1 where 1 is most severe
 
 python input_signal.py $DISTANCE $TIME $NS 'component_params.csv'
-python radio.py '1.0' '1.0' $ANGLE $DISTANCE $WEATHER 'input_signal.csv'
+python radio.py '2.0' '1.5' $ANGLE $DISTANCE $WEATHER 'input_signal.csv'
